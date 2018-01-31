a684d488a11bcb0ccf7a7ec5c8df720209979ad9
Muroz <dazc35@mun.ca>
2018-01-31 09:45:32 -0330

    Recent activity timer implemented

--- routes/voice.js
+++ routes/voice.js
@@ -10,47 +10,48 @@ var Visit= require('./../models/visit');
 // HTTP POST to /voice in our application
 router.post('/', function(req,res) {
 
-  const twiml = new VoiceResponse();
-  //twiml.say({ voice: 'alice' }, 'Welcome to the Peachy service! Stay in line to receive the care plan for this visit');
+//   const twiml = new VoiceResponse();
+//   //twiml.say({ voice: 'alice' }, 'Welcome to the Peachy service! Stay in line to receive the care plan for this visit');
 
-/** helper function to set up a <Gather> */
-  function gather() {
-    const gatherNode = twiml.gather({ numDigits: 7, timeout: 15});
-    gatherNode.say({ voice: 'alice' }, 'Welcome to the Peachy service! Please press the visit id');
+// /** helper function to set up a <Gather> */
+//   function gather() {
+//     const gatherNode = twiml.gather({ numDigits: 7, timeout: 15});
+//     gatherNode.say({ voice: 'alice' }, 'Welcome to the Peachy service! Please press the visit id');
 
-    // If the user doesn't enter input, loop
-    twiml.redirect('/voice');
-  }
+//     // If the user doesn't enter input, loop
+//     twiml.redirect('/voice');
+//   }
 
-    console.log(req);
-    var today = new Date();
-    console.log(today.toDateString() == 'Mon Jan 15 2018');
+//     console.log(req);
+//     var today = new Date();
+//     console.log(today.toDateString() == 'Mon Jan 15 2018');
 
   // If the user entered digits, process their request
-  if (req.body.Digits.length == 7) {
-    // var today = new Date();
-    // console.log(today.toDateString())
-    // var idString = today.toDateString();
-    // idString = idString.replace(/\s+/g, '');
-    // var idedString = idString + req.body.Caller;
+  // if (req.body.Digits.length == 7) {
+  //   // var today = new Date();
+  //   // console.log(today.toDateString())
+  //   // var idString = today.toDateString();
+  //   // idString = idString.replace(/\s+/g, '');
+  //   // var idedString = idString + req.body.Caller;
 
-    // Visit.findOne({'visitid':idedString, 'client':req.body.Digits}, function(err, visit){
-    //   if(err) return handleError(err);
+  //   // Visit.findOne({'visitid':idedString, 'client':req.body.Digits}, function(err, visit){
+  //   //   if(err) return handleError(err);
 
-    //   console.log(visit);
-    // })
-    console.log(req.body.Digits);
-  } else {
-    // If no input was sent, use the <Gather> verb to collect user input
-    gather();
-  }
+  //   //   console.log(visit);
+  //   // })
+  //   console.log(req.body.Digits);
+  // } else {
+  //   // If no input was sent, use the <Gather> verb to collect user input
+  //   gather();
+  // }
 
-  console.log('here')
-  console.log(req.body.Digits);
+  // console.log('here')
+  // console.log(req.body.Digits);
 
   // Render the response as XML in reply to the webhook request
   res.type('text/xml');
-  res.send(twiml.toString());
+  //res.send(twiml.toString());
+  res.send();
 
 });
 
--- src/components/dashboard/activityView.js
+++ src/components/dashboard/activityView.js
@@ -2,13 +2,33 @@
 import React from 'react';
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
+import socketIOClient from "socket.io-client";
+import { endianness } from 'os';
+import { setImmediate, setInterval } from 'timers';
 
 
 class ActivityView extends React.Component{
+    constructor() {
+        super();
+        this.state = {
+          timer: null
+        };
+      }
+
+      componentDidMount() {
+        let timer = setInterval(this.tick, 1000);
+        this.setState({timer});
+      }
+      componentWillUnmount(){
+        this.clearInterval(this.state.timer);
+      }
+      tick(){
+        console.log('ticking');
+      }
 render(){
     return(
-        <div>
-            Activity view goes here
+        <div style={{ textAlign: "center" }}>
+          Here comes the info    
         </div>
     )
   }
